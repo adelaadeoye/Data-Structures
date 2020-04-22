16d451027550d74a8878969cc9532e5d58aa16c5
Adeoye Adela <adeoye.adela@gmail.com>
2020-04-22 22:06:47 +0100

    BST day one done

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -12,21 +12,64 @@ class BinarySearchTree:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        # for us to call `insert` on them 
+      if value < self.value:
+          # check if self.left is a valid node 
+          if self.left:
+              self.left.insert(value)
+          # the left side is empty 
+          else:
+              # we've found a valid parking spot 
+              self.left = BinarySearchTree(value)
+      # otherwise, value >= self.value
+      else:
+          if self.right:
+              self.right.insert(value)
+          else:
+              self.right = BinarySearchTree(value)
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        # check if target is equal to value
+        if target == self.value:
+            return True
 
+        else:
+            if target< self.value:
+                if self.left is None:
+                    return False
+                else:
+                    return self.left.contains(target)
+            else:
+                if self.right is None:
+                    return False
+                else:
+                    return self.right.contains(target)
+            
+
+       
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        
+       max_no = self.value
+       if self.right:
+           return self.right.get_max()
+       else:
+           return max_no
+
+    
+
+
 
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
     def for_each(self, cb):
-        pass
+        cb(self.value)
+        if self.right:
+            self.right.for_each(cb)
+        if self.left:
+            self.left.for_each(cb)
 
     # DAY 2 Project -----------------------
 
